588a0c2ac80d04e766e6eca1729cb2b49c65ed3c
itsme <itsme@xs4all.nl>
2023-07-22 19:33:14 +0200

    download new files found in manifest files.

---
+++ dn-assets.sh
@@ -0,0 +1,8 @@
+# downloads all assets from web.whatsapp.com mentioned in assets-manifest files, and not yet present.
+for f in web.whatsapp.com/assets-manifest-*.json ; do
+    jq < "$f" | perl -nle 'if (/"(\S+)":/) { print($1); }'
+done | grep -v "^inline-" | while read f; do
+    if [[ ! -e "web.whatsapp.com/$f" ]]; then
+        get -m HEAD "https://web.whatsapp.com/$f" | grep -q "^HTTP/1.1 200"  && echo  "https://web.whatsapp.com/$f"
+    fi
+done | x0  wget -U "Mozilla/5.0" -x -nc
