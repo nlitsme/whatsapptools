627687520b60d9ff40c53e62ff323489ea9340db
itsme <itsme@xs4all.nl>
2023-07-22 19:33:01 +0200

    scan for new manifest files

---
+++ scan-assets.sh
@@ -0,0 +1,15 @@
+# discovering manifest files by enumerating all version nrs, 
+for ((a=2300 ; a<2400 ; a++)); do
+    for ((b=0 ; b<25 ; b++)); do
+        f="web.whatsapp.com/assets-manifest-2.$a.$b.json"
+        if [[ ! -e "$f" ]]; then
+            get -H "User-Agent: Mozilla/5.0" -m HEAD "https://$f" | grep -q "^HTTP/1.1 200"  && wget -U "Mozilla/5.0" -x -nc "https://$f"
+        fi
+        f="web.whatsapp.com/binary-transparency-manifest-2.$a.$b.json"
+        if [[ ! -e "$f" ]]; then
+            get -H "User-Agent: Mozilla/5.0" -m HEAD "https://$f" | grep -q "^HTTP/1.1 200"  && wget -U "Mozilla/5.0" -x -nc "https://$f"
+        fi
+
+    done
+done
+exit 0
